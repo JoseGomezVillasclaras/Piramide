8a842fd533c8f4a0162da6e81b4394d8841e04e4
JoseGomezVillasclaras <jose.gomez.villasclaras@gmail.com>
2023-04-24 20:54:16 +0200

    push_24

--- flex2.js
+++ flex2.js
@@ -1,6 +1,6 @@
 'use strict'
 //Pedimos el nº de cuadro donde escribir
-
+/*
 var numPos = prompt("Diga el cuadro en el que quiere dibujar(1 a 5)", "");
 var a = prompt("Diga el color en el que quiere la piramide: 1 Azul, 2 Rojo, 3 Verde", "");
 numPos = parseInt(numPos);
@@ -26,7 +26,101 @@ for (let i = 1; i <= height; i++) {
     }
     output += row.padStart(Math.floor((height + row.length) / 2), " ") + "<br>";
 }
+*/
+// La cosa era asignar una funcion para cambiar el color del cuadro en el onclick
+//Arrays
+//var colorFr=['yellow','red','blue','pink','violet']
 
+var colorFr= new Array('yellow','red','blue','pink','violet');
+/*
+colorFn[0] = 'grey';
+colorFn[1] = 'blue';
+colorFn[2] = 'black';
+colorFn[3] = 'yellow';
+colorFn[4] = 'red';
+*/
+var colorFn = new Array(
+    'white',
+    'blue',
+    'black',
+    'yellow',
+    'red'
+)
+
+function pinturaInicial() {
+    for ( var i = 0; i < 6; i++) {
+        let identificador="c"+(i+1);
+        let elemento = document.getElementById(identificador)
+        elemento.style.backgroundColor=colorFn[i];
+        elemento.style.color=colorFn[i];
+    }
+}
+
+var color = 'white';
+/*
+function eligeColor() {
+    color = prompt("Di que color quieres que sea el saludo(Rojo, Azul, Verde , Rosa)")
+    switch (color) {
+        case 'Rojo':
+            color = 'red';
+            break;
+        case 'Azul':
+            color = 'blue';
+            break;
+        case 'Verde':
+            color = 'green';
+            break;
+        case 'Rosa':
+            color = 'pink';
+            break;
+        default:
+            color = 'black'; //Lo pongo en caso de error
+            break;
+
+    }
+}
+*/
+function cambiarColor(identificador) {
+    let id = "c" + identificador;
+    let cuadro = document.getElementById(id);
+    cuadro.style.color = color;
+
+    let a;
+    a++;
+    switch (a) {
+        case 1:
+
+            color = 'blue';
+            break;
+        case 2:
+
+            console.log("abcda");
+            cuadro.style.color = 'red';
+            break;
+        case 3:
+            cuadro = 'green';
+            break;
+        default:
+            color = 'blue'; //Lo pongo en caso de error
+            break;
+    }
+
+    cuadro.innerHTML = "Hola";
+
+
+
+}
+function saludar(identificador) {
+    let id = "c" + identificador;
+    let cuadro = document.getElementById(id);
+
+    cuadro.innerHTML = "Hola";
+}
+
+pinturaInicial();
+
+eligeColor();
+/*
 switch(a){
 case 1:
     esteCuadro.innerHTML = output;
@@ -41,3 +135,4 @@ case 3:
     esteCuadro.style.color="green"
 break;
 }
+*/
